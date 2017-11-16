6c404e2817f7cce1f0c9a996f487bc9a4de6f334
grubbycc <1573976179@qq.com>
2017-11-16 09:34:52 +0800

    update

--- 核心部分/11_常见模块.py
+++ 核心部分/11_常见模块.py
@@ -33,6 +33,6 @@ m.update("hello".encode("utf-8"))
 # 返回十六进制数字字符串
 print(m.hexdigest())  # 5d41402abc4b2a76b9719d911017c592
 # 不管如何更新hash对象,最终返回的都是32位长度的一串数值
-m.update("jhsahdskdsadaldwsandadasndal".encode("utf-8"))
+m.update("jhsahdskdsadaldw".encode("utf-8"))
 print(m.hexdigest())  # 0bec77eeeedd61ea5db95c0202b68d45
 
