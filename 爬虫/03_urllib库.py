2b89aeb3ec26e2b9ccf398d5b2f3fe5698e44857
chenqian <chenqian@qmtv.comm>
2018-01-25 09:47:55 +0800

    update

--- 爬虫/03_urllib库.py
+++ 爬虫/03_urllib库.py
@@ -1,12 +1,6 @@
 import urllib.request
 import urllib.parse
 
-"""
-urllib.parse模块的urlencode()方法可以将字典键值对按url编码转换,从而能被web服务器接收
-urlencode(): 编码   -- 将key:value键值对转换成"key=value"字符串
-unquote(): 解码
-"""
-
 url = "http://www.baidu.com/s"
 keyword = input("请输入要搜索的关键字: ")
 wd = {"wd": keyword}
